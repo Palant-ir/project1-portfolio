18092ef52ef9aea487348a885105c09ecc910375
han <johnmfdoe42@gmail.com>
2023-12-13 16:42:58 +0800

    extracted ContactPage class to new file

---
+++ lib/contact_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({super.key});
+  final TextStyle textStyle = const TextStyle(fontSize: 20.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact Info'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Card(
+              elevation: 6,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const Icon(Icons.email),
+                    title: Text(
+                      'han.kaungkhant42@gmail.com',
+                      style: textStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.phone),
+                    title: Text(
+                      '+65 9180 8539',
+                      style: textStyle,
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.apps),
+                    title: Text(
+                      'github : @sumerAk',
+                      style: textStyle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 40),
+            const Placeholder(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/contact_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -70,57 +71,6 @@ class MyPortfolioApp extends StatelessWidget {
               style: textStyle,
               textAlign: TextAlign.start,
             )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ContactPage extends StatelessWidget {
-  const ContactPage({super.key});
-  final TextStyle textStyle = const TextStyle(fontSize: 20.0);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Contact Info'),
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Card(
-              elevation: 6,
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: const Icon(Icons.email),
-                    title: Text(
-                      'han.kaungkhant42@gmail.com',
-                      style: textStyle,
-                    ),
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.phone),
-                    title: Text(
-                      '+65 9180 8539',
-                      style: textStyle,
-                    ),
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.apps),
-                    title: Text(
-                      'github : @sumerAk',
-                      style: textStyle,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 40),
-            const Placeholder(),
           ],
         ),
       ),
